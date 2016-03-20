3374ff02361fb714d0c9cd62201768831d950bc3
aniaw <ania_w@outolook.com>
2016-03-20 16:17:03 +0100

    add logout function to CarPortal

--- app/app.js
+++ app/app.js
@@ -5,8 +5,20 @@
         Car: function (model, year) {
             this.model = model;
             this.year = year;
+        },
+        CarPortal: function () {
+            this.credentials = {};
+            this.login = function (username, password) {
+                this.credentials.username = username;
+                this.credentials.password = password;
+            };
         }
     }
 })();
+
+
+
+
+
 
 
