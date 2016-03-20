f9bc396ebb4ea5fbcb5c48ff2c1fabe94a1eefb7
aniaw <ania_w@outolook.com>
2016-03-20 16:16:13 +0100

    add credential log to CarPortal test

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -43,6 +43,7 @@ describe('app', function () {
             expect(carPortal.credentials).toEqual({});
         });
         it('should set username to maria and password to qwerty when login is call', function () {
+            console.log(carPortal.credentials);
             carPortal.login('maria', 'qwerty');
             expect(carPortal.credentials).toEqual({username: 'maria', password: 'qwerty'});
         });
