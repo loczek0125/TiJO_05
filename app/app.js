f0ceafdbfa618cd1cf715ffc71c947ddafcf82db
Paulina Sak <paulinasak0@gmail.com>
2016-04-08 19:47:43 +0200

    class car

--- app/app.js
+++ app/app.js
@@ -5,23 +5,8 @@
         Car: function (model, year) {
             this.model = model;
             this.year = year;
-        },
-        CarPortal: function () {
-            this.credentials = {};
-            this.login = function (username, password) {
-                this.credentials.username = username;
-                this.credentials.password = password;
-            };
-            this.logout = function () {
-                this.credentials = {};
-            };
         }
     }
 })();
-
-
-
-
-
 
 
