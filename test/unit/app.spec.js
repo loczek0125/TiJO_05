9d453e2072f7293ece4a28bca8544a064d727e45
Paulina Sak <paulinasak0@gmail.com>
2016-04-08 19:01:07 +0200

    test

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -3,54 +3,51 @@ describe('app', function () {
 
     var app = window.app;
 
-    describe('generateMessage', function () {
+    var car;
+    xdescribe('Car', function () {
+        beforeEach(function () {
+            car = new app.Car('Audi', 1998);
+        });
+        describe('property', function () {
+            it('should defined model', function () {
+                expect(car.model).toBeDefined();
+            });
+            it('should defined year', function () {
+                expect(car.year).toBeDefined();
+            });
+        });
+        describe('value', function () {
+            it('should be set to Audi on model property', function () {
+                expect(car.model).toBe('Audi');
+            });
 
+            car = new app.Car('BMW', 2000);
+
+            it('should be set to 1998 on year property', function () {
+                expect(car.year).toBe(1998);
+            });
+        });
     });
 
-    describe('isPalindrome', function () {
-
-        describe('toHaveBeenCalled', function () {
-
+    var carPortal;
+    describe('CarPortal', function () {
+        carPortal = new app.CarPortal();
+        afterEach(function () {
+            carPortal.logout();
         });
-
-        describe('and.callThrough', function () {
-
+        it('should set username to john and password to 12345 when login is call', function () {
+            carPortal.login('john', '12345');
+            expect(carPortal.credentials).toEqual({username: 'john', password: '12345'});
         });
-
-        describe('and.returnValue', function () {
-
+        it('should clean credentials after each it', function () {
+            expect(carPortal.credentials).toEqual({});
         });
-
-        describe('and.callFake', function () {
-
+        it('should set username to maria and password to qwerty when login is call', function () {
+            carPortal.login('maria', 'qwerty');
+            expect(carPortal.credentials).toEqual({username: 'maria', password: 'qwerty'});
         });
-
-        describe('calls.count()', function () {
-
-        });
-    });
-
-    describe('vowelCount', function () {
-
-        describe('toHaveBeenCalled', function () {
-
-        });
-
-        describe('and.callThrough', function () {
-
-        });
-
-        describe('and.returnValue', function () {
-
-        });
-
-        describe('and.callFake', function () {
-
-        });
-
-        describe('calls.count()', function () {
-
+        it('should clean credentials after each it', function () {
+            expect(carPortal.credentials).toEqual({});
         });
     });
 });
-
